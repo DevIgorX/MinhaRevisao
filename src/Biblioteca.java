b539c742cd3b2ee8b6eabfb9dd145287ae44fc45
Tauam Igor <tauamigor@gmail.com>
2024-04-15 23:32:01 -0300

    minha revisao

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class Biblioteca {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// Definição da classe Livro
+class Livro {
+    private String titulo;
+
+    public Livro(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+}
+
+// Definição da classe Biblioteca
+public class Biblioteca {
+    private ArrayList<Livro> livros = new ArrayList<>();
+
+    public Biblioteca() {
+        // Adicionando 10 livros à lista
+        livros.add(new Livro("Livro 1"));
+        livros.add(new Livro("Livro 2"));
+        livros.add(new Livro("Livro 3"));
+        livros.add(new Livro("Livro 4"));
+        livros.add(new Livro("Livro 5"));
+        livros.add(new Livro("Livro 6"));
+        livros.add(new Livro("Livro 7"));
+        livros.add(new Livro("Livro 8"));
+        livros.add(new Livro("Livro 9"));
+        livros.add(new Livro("Livro 10"));
+    }
+
+    public void mostrarLivros() {
+        System.out.println("Lista de Livros Disponíveis:");
+        for (int i = 0; i < livros.size(); i++) {
+            System.out.println((i + 1) + ". " + livros.get(i).getTitulo());
+        }
+    }
+
+    public void emprestarLivro(String nomeSolicitante) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá, " + nomeSolicitante + "! Por favor, selecione o número do livro que deseja emprestar:");
+        int escolha = scanner.nextInt();
+
+        if (escolha >= 1 && escolha <= livros.size()) {
+            Livro livroSelecionado = livros.get(escolha - 1);
+            System.out.println("Você selecionou o livro: " + livroSelecionado.getTitulo());
+        } else {
+            System.out.println("Opção inválida!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem-vindo à Biblioteca! Por favor, digite seu nome:");
+        String nomeSolicitante = scanner.nextLine();
+
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.mostrarLivros();
+        biblioteca.emprestarLivro(nomeSolicitante);
+    }
 }
--- src/Carro.java
+++ src/Carro.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Carro {
+public class Carro {
+    private int consumo;
+    private int qdtTanque;
+
+
+    public Carro(int consumo){
+        this.consumo = consumo;
+        this.qdtTanque = 0;
+    }
+
+    public void adicionarGasolina(int quantidade){
+        this.qdtTanque += quantidade;
+    }
+
+    public int obterGasolina(){
+        return qdtTanque;
+    }
+    public void setQdtTanque(int tanque){
+        this.qdtTanque = tanque;
+    }
+
+
+    public void andar(int km){
+        int calculo = obterGasolina() - (km / this.consumo);
+        setQdtTanque(calculo);
+
+    }
+
+    public static void main(String[] args) {
+
+        Carro meuCarro = new Carro(10);
+        meuCarro.adicionarGasolina(30);
+        System.out.println(meuCarro.obterGasolina() + " Litros no tanque");
+        meuCarro.andar(200);
+        System.out.println(meuCarro.obterGasolina());
+
+    }
+
+
 }
